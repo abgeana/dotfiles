8796cd3b51c8518363e5222a9a7c29de25f27606
alegen <alex@alegen.net>
2015-06-28 23:00:57 +0200

    ipython config update

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -6,51 +6,49 @@
         2) copy this file to ~/.ipython/profile_default
 '''
 
-import subprocess
-
-# helper functions
-def whoami():
-    p = subprocess.Popen('whoami', stdout = subprocess.PIPE, stderr=subprocess.PIPE)
-    out, _ = p.communicate()
-    out = out.replace('\n', '')
-    return out
-
 # get Config object
 c = get_config()
 
-# do not display the banner
-c.TerminalIPythonApp.display_banner = False
-
-# startup execution of stuff
-c.InteractiveShellApp.exec_lines = [
+###
+#   interactive shell configs
+###
+c.InteractiveShell.autoindent = True        # auto-indentation where appropriate
+c.InteractiveShell.confirm_exit = False     # exit without asking
+c.InteractiveShell.editor = 'vim'           # vim as editor
+c.InteractiveShellApp.exec_lines = [        # startup execution of stuff
+    'import numpy as np',
+    'import os, os.path',
+    'import sys'
 ]
 
-c.InteractiveShellApp.exec_files = [
+###
+#   terminal app configs
+###
+c.TerminalIPythonApp.display_banner = False # do not display the banner
+
+###
+#   aliases
+###
+
+c.AliasManager.user_aliases = [
+    ('lss', 'ls --color=tty -Alh')
 ]
 
-# auto-indentation where appropriate
-c.InteractiveShell.autoindent = True
+###
+#   the prompt
+###
 
 # color variables to make life easier
-cLCyan = '{color.LightCyan}'
-cLBlue = '{color.LightBlue}'
-cRed = '{color.Red}'
-cGreen = '{color.Green}'
-cBlue = '{color.Blue}'
-cWhite = '{color.White}'
-
-# root gets green, plebs get red
-if whoami() == 'root':
-    cUser = cGreen
-else:
-    cUser = cRed
-
-# and the hostname appears white
-cHost = cWhite
+cLCyan  = '{color.LightCyan}'
+cLBlue  = '{color.LightBlue}'
+cRed    = '{color.Red}'
+cGreen  = '{color.Green}'
+cBlue   = '{color.Blue}'
+cWhite  = '{color.White}'
 
 # prompt variables
 prompt_start = cLCyan + '[IP] '
-c.PromptManager.in_template = prompt_start + cUser + '\u' + cGreen + '  @  ' + cHost + '\h' + cGreen + '  : \Y0' + cLCyan + ' [\#' + cLCyan + '] '
-c.PromptManager.in2_template = cGreen + '>> '
-c.PromptManager.out_template = cLCyan + '[\#' + cLCyan + '] '
+c.PromptManager.in_template = cLCyan + 'IPython ' + cGreen + '[\#' + cGreen + ']' + cLCyan + ' <<< '
+c.PromptManager.in2_template = cLCyan + '<<< '
+c.PromptManager.out_template = cGreen + '[\#' + cGreen + ']' + cLCyan + ' >>> '
 c.PromptManager.justify = True
