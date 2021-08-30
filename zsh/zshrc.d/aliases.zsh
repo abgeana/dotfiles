6fd6d88dfa632879fa41183bd82d564be920b5e9
Alexandru B. Geană <ab@geana.ro>
2021-08-30 12:28:05 +0000

    add ip alias with color

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -22,6 +22,7 @@ alias ncdu='ncdu -0 -e --color dark'
 alias rcp="rsync -ah --inplace --info=progress2"
 alias rm='echo "This is not the command you are looking for."; false'
 alias pz='ps x -o "%p %r %u %c"'
+alias ip='ip --color=auto'
 
 # global aliases defined with '-g' (i.e. can be placed anywhere in other commands)
 alias -g silent='> /dev/null 2>&1'
