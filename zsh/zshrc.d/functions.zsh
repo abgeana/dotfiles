02631c78a455e4ee3dcd82e7f31da560a9c0be10
Alexandru Geana <ab@geana.io>
2021-09-20 22:37:04 +0200

    add zsh command to test 24bit color support

--- zsh/zshrc.d/functions.zsh
+++ zsh/zshrc.d/functions.zsh
@@ -53,6 +53,24 @@ show-colors() {
     echo -e "\e[m"
 }
 
+# this function prints a test pattern for 24bit colors
+# https://unix.stackexchange.com/a/404415
+test-24bit-colors() {
+    awk -v term_cols="${width:-$(tput cols || echo 80)}" 'BEGIN{
+        s="/\\";
+        for (colnum = 0; colnum<term_cols; colnum++) {
+            r = 255-(colnum*255/term_cols);
+            g = (colnum*510/term_cols);
+            b = (colnum*255/term_cols);
+            if (g>255) g = 510-g;
+            printf "\033[48;2;%d;%d;%dm", r,g,b;
+            printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+            printf "%s\033[0m", substr(s,colnum%2+1,1);
+        }
+        printf "\n";
+    }'
+}
+
 # this function calcultates the size of all directories and files
 # in the current working directory
 sz() {
