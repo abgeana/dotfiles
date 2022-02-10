3b91033a50610993d7d4dfac3bbcf5a46938193e
Alexandru Geana <ab@geana.io>
2022-02-10 21:26:43 +0100

    configure ssh and gpg for yubikeys

--- zsh/zshrc.d/vars.zsh
+++ zsh/zshrc.d/vars.zsh
@@ -8,14 +8,6 @@ export HISTFILESIZE=
 # zsh binary distro independent location (needed for .tmux.common)
 export ZSH_BINARY=$(which zsh 2> /dev/null)
 
-# needed for gpg-agent (see man gpg-agent)
-export GPG_TTY=$(tty)
-if [[ -f "$HOME/.gpg-agent-info" ]]; then
-    . "$HOME/.gpg-agent-info"
-    export GPG_AGENT_INFO
-    export SSH_AUTH_SOCK
-fi
-
 # for $EDITOR, check for availability of neovim, vimx, or otherwise fallback to vim
 NVIM_BIN=$(which nvim 2> /dev/null) # check for neovim
 if [[ $? == 0 ]]; then
@@ -36,6 +28,13 @@ fi
 if [[ ! -v EDITOR ]]; then
     export EDITOR=vi # just use vi
 fi
+
+# needed for gpg-agent (see man gpg-agent)
+export GPG_TTY=$(tty)
+
+# export the SSH_AUTH_SOCK variable used by the ssh agent to point it to the gpg agent
+# this is needed to use the hardware gpg keys generated on yubikeys
+export SSH_AUTH_SOCK=`gpgconf --list-dirs agent-ssh-socket`
 
 # custom PATH
 PATH=""
