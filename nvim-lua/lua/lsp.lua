5cece9a5310755bd3c3d7186e29a811bdc90e47c
Alexandru Geana <ab@geana.io>
2022-05-11 20:53:07 +0200

    initial autocomplete via lsp config

--- nvim-lua/lua/lsp.lua
+++ nvim-lua/lua/lsp.lua
@@ -1,12 +1,162 @@
 local nvim_lsp = require('lspconfig')
+local cmp = require('cmp')
+local ok, lspkind = pcall(require, "lspkind")
+if not ok then
+  return
+end
+
+lspkind.init()
 
 local on_attach = function(client, bufnr)
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-    -- enable completion via the lsp
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
+
+cmp.setup({
+    -- the arguments below are documented by the project
+    -- https://github.com/hrsh7th/nvim-cmp/blob/a226b6a4ff72e5e809ed17734318233fb25c87f3/doc/cmp.txt#L360-L362
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)            -- For vsnip users.
+            -- require('luasnip').lsp_expand(args.body)     -- For luasnip users.
+            -- require'snippy'.expand_snippet(args.body)    -- For snippy users.
+            -- vim.fn["UltiSnips#Anon"](args.body)          -- For ultisnips users.
+        end,
+    },
+
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+
+    mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-e>"] = cmp.mapping.abort(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' },           -- For vsnip users.
+        -- { name = 'luasnip' },      -- For luasnip users.
+        -- { name = 'snippy' },       -- For snippy users.
+        -- { name = 'ultisnips' },    -- For ultisnips users.
+    }, {
+        { name = 'buffer' },
+    }),
+
+    formatting = {
+        format = lspkind.cmp_format {
+            with_text = true,
+            menu = {
+                buffer = "[buf]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[api]",
+                path = "[path]",
+                luasnip = "[snip]",
+                gh_issues = "[issues]",
+                tn = "[TabNine]",
+            },
+        }
+    },
+
+    experimental = {
+        native_menu = false
+    }
+})
+
+-- setup cmdline configuration for search commands (/)
+-- cmp.setup.cmdline('/', {
+--     mapping = cmp.mapping.preset.cmdline({
+--         -- the snippets below are adapted from the code of nvim-cmp itself
+--         -- https://github.com/hrsh7th/nvim-cmp/blob/f51dc68e1bb170fc49c2d7e13eb45e5ec83f5ee9/lua/cmp/config/mapping.lua#L69-L88
+--         ["<C-j>"] = {
+--             c = function(fallback)
+--                 local cmp = require('cmp')
+--                 if cmp.visible() then
+--                     cmp.select_next_item()
+--                 else
+--                     fallback()
+--                 end
+--             end,
+--         },
+--         ["<C-k>"] = {
+--             c = function(fallback)
+--                 local cmp = require('cmp')
+--                 if cmp.visible() then
+--                     cmp.select_prev_item()
+--                 else
+--                     fallback()
+--                 end
+--             end,
+--         },
+--         ["<CR>"] = {
+--             c = function(fallback)
+--                 local cmp = require('cmp')
+--                 if cmp.visible() then
+--                     cmp.confirm({ select = true })
+--                 else
+--                     fallback()
+--                 end
+--             end,
+--         },
+--     }),
+--     sources = {
+--         { name = 'buffer' }
+--     }
+-- })
+
+-- -- setup cmdline configuration for normal commands (:)
+-- cmp.setup.cmdline(':', {
+--     mapping = cmp.mapping.preset.cmdline({
+--         -- the snippets below are adapted from the code of nvim-cmp itself
+--         -- https://github.com/hrsh7th/nvim-cmp/blob/f51dc68e1bb170fc49c2d7e13eb45e5ec83f5ee9/lua/cmp/config/mapping.lua#L69-L88
+--         ["<C-j>"] = {
+--             c = function(fallback)
+--                 local cmp = require('cmp')
+--                 if cmp.visible() then
+--                     cmp.select_next_item()
+--                 else
+--                     fallback()
+--                 end
+--             end,
+--         },
+--         ["<C-k>"] = {
+--             c = function(fallback)
+--                 local cmp = require('cmp')
+--                 if cmp.visible() then
+--                     cmp.select_prev_item()
+--                 else
+--                     fallback()
+--                 end
+--             end,
+--         },
+--         ["<CR>"] = {
+--             c = function(fallback)
+--                 local cmp = require('cmp')
+--                 if cmp.visible() then
+--                     cmp.confirm({
+--                         behavior = cmp.ConfirmBehavior.Replace,
+--                         select = true
+--                     })
+--                 else
+--                     fallback()
+--                 end
+--             end,
+--         },
+--     }),
+--     sources = cmp.config.sources({
+--         { name = 'path' }
+--     }, {
+--         { name = 'cmdline' }
+--     })
+-- })
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 function lsp_setup()
     -- disable inline buffer error messages (linting)
@@ -20,6 +170,7 @@ function lsp_setup()
     )
 
     nvim_lsp.ccls.setup {
+        capabilities = capabilities,
         on_attach = on_attach,
         flags = {
             debounce_text_changes = 150,
@@ -27,6 +178,7 @@ function lsp_setup()
     }
 
     nvim_lsp.rust_analyzer.setup {
+        capabilities = capabilities,
         on_attach = on_attach,
         flags = {
             debounce_text_changes = 150,
