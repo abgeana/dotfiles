c3bc51e883fdfa637e80f66788baea8f6c5e994a
Alexandru Geana <ab@geana.io>
2022-05-11 20:53:19 +0200

    changes to the neovim colorscheme

--- nvim-lua/lua/colorscheme.lua
+++ nvim-lua/lua/colorscheme.lua
@@ -4,6 +4,10 @@ if vim.fn.empty(vim.fn.glob('~/.config/nvim/plugged/vim-colors-solarized/colors/
     vim.cmd [[
         autocmd colorscheme solarized highlight colorcolumn ctermbg=1
         colorscheme solarized
+        hi StatusLine cterm=reverse ctermfg=0 ctermbg=2 gui=bold,reverse
+        hi StatusLineNC cterm=reverse ctermfg=0 ctermbg=3 gui=bold,reverse
+        hi VertSplit ctermfg=11 ctermbg=0 gui=reverse
+        set fillchars+=vert:\ " white space at the end
     ]]
 else
     vim.cmd [[
