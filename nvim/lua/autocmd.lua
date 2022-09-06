17c72809c56c4a31857de71d74ada3ae3f11eb0b
Alexandru Geana <ab@geana.io>
2022-09-06 20:29:50 +0200

    Revert "nvim: changes to viewoptions and keeping folds in sync"
    This reverts commit 2ea47478e5386432ec0788ec929bb69bd040b763.

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -3,7 +3,3 @@ vim.cmd('autocmd BufWinEnter * :set relativenumber number')
 
 -- set indentation of 2 spaces for yaml files
 vim.cmd('autocmd FileType yaml IndentWidth 2')
-
--- use this to keep folds in sync between different windows displaying the same buffer
-vim.cmd('autocmd BufLeave * :mkview')
-vim.cmd('autocmd WinLeave * :mkview')
