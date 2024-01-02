e775be3fd966ecdbe68813f7353c502d8e8526b0
Alexandru Geana <ab@geana.io>
2024-01-02 18:10:48 +0100

    [nvim] simplify nvim-cmp mappings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -39,10 +39,7 @@ return {
                 mapping = cmp.mapping.preset.insert {
                     ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
                     ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-                    ['<C-e>'] = cmp.mapping.abort(),
                     ['<Esc>'] = cmp.mapping.abort(),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<CR>'] = cmp.mapping.confirm { select = false },
                 },
