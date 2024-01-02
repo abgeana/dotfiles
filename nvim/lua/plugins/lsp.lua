af8fc636e7a2883e6e4c9624cf670b55d50207aa
Alexandru Geana <ab@geana.io>
2024-01-02 10:47:19 +0100

    [nvim] update cmp bindings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,10 +40,11 @@ return {
                     ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
                     ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
                     ['<C-e>'] = cmp.mapping.abort(),
+                    ['<Esc>'] = cmp.mapping.abort(),
                     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm { select = true },
+                    ['<CR>'] = cmp.mapping.confirm { select = false },
                 },
 
                 sources = cmp.config.sources(
