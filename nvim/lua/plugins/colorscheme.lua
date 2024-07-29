f904103d714bff7ac5906b116ff7d393e07de49a
Alexandru Geana <ab@geana.io>
2024-07-29 23:27:40 +0200

    [nvim] different solarized plugin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,33 @@
 return {
-    'svrana/neosolarized.nvim',
+    'maxmx03/solarized.nvim',
     lazy = false,
     priority = 1000,
 
     config = function()
-        require('neosolarized').setup {
-            comment_italics = false,
-        }
+        my_style = { italic = false, standout = false, bold = false }
+
+        require('solarized').setup({
+            highlights = function (colors, colorhelper)
+                return {
+                    Visual = my_style,
+                }
+            end,
+
+            styles = {
+                -- disable italics for everything
+                -- https://github.com/maxmx03/solarized.nvim/blob/6875d609077411c88d293cb0520ca4e08b829ded/lua/solarized/config.lua#L17-L26
+                comments = my_style,
+                functions = my_style,
+                variables = my_style,
+                numbers = my_style,
+                constants = my_style,
+                parameters = my_style,
+                keywords = my_style,
+                types = my_style,
+            },
+        })
 
         vim.o.background = 'dark'
-        vim.o.termguicolors = true
-        vim.cmd("colorscheme neosolarized")
+        vim.cmd.colorscheme 'solarized'
     end,
-
-    dependencies = {
-        'tjdevries/colorbuddy.nvim',
-    },
 }
