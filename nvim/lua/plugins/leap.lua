f0476cfd085aec3db4526d210ee4ea5bf27ea7b5
Alexandru Geana <git@geana.io>
2026-01-15 15:24:05 +0100

    [nvim] update leap config for new api

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -2,7 +2,8 @@ return {
     'ggandor/leap.nvim',
 
     config = function()
-        require('leap').create_default_mappings()
-        vim.cmd("hi LeapLabel guifg=Black guibg=#87ff00")
+        vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+        vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+        vim.cmd('hi LeapLabel guifg=Black guibg=#87ff00')
     end,
 }
